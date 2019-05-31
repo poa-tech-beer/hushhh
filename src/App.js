5d1e827f08bc71cc407a42e212c8b6476767b250
Adrien Denat <adrien.denat@gmail.com>
2019-05-31 00:12:36 -0300

    feat: add Prettier format on save support

--- src/App.js
+++ src/App.js
@@ -1,59 +1,56 @@
-import React from 'react'
-import Peer from 'peerjs'
-import Layout from './components/Layout'
+import React from "react"
+import Peer from "peerjs"
+import Layout from "./components/Layout"
 
 export const AppContext = React.createContext()
 
 // See https://github.com/peers/peerjs
 // See https://github.com/jmcker/Peer-to-Peer-Cue-System/blob/master/send.html
 const peer = new Peer(null, {
-  debug: 2
-});
+  debug: 2,
+})
 
-var lastPeerId = null;
-
+var lastPeerId = null
 
 /**
  * Connected event handler.
  */
-peer.on('open', function (id) {
+peer.on("open", function(id) {
   // Workaround for peer.reconnect deleting previous id
   if (peer.id === null) {
-    console.log('Received null id from peer open');
-    peer.id = lastPeerId;
+    console.log("Received null id from peer open")
+    peer.id = lastPeerId
+  } else {
+    lastPeerId = peer.id
   }
-  else {
-    lastPeerId = peer.id;
-  }
-  console.log('ID: ' + peer.id);
-});
+  console.log("ID: " + peer.id)
+})
 
 /**
  * Disconnected event handler.
  */
-peer.on('disconnected', function () {
-  console.log('Connection lost. Please reconnect');
+peer.on("disconnected", function() {
+  console.log("Connection lost. Please reconnect")
   // Workaround for peer.reconnect deleting previous id
-  peer.id = lastPeerId;
-  peer._lastServerId = lastPeerId;
-    peer.reconnect();
-});
+  peer.id = lastPeerId
+  peer._lastServerId = lastPeerId
+  peer.reconnect()
+})
 
 /**
  * Closed (destroyed) connexion.
  */
-peer.on('close', function() {
-  console.log('Connection destroyed');
-});
+peer.on("close", function() {
+  console.log("Connection destroyed")
+})
 
 /**
  * Error event handler.
  */
-peer.on('error', function (err) {
-  console.log(err);
-  alert('' + err);
-});
-
+peer.on("error", function(err) {
+  console.log(err)
+  alert("" + err)
+})
 
 const App = ({ element, props }) => {
   return (
@@ -62,6 +59,5 @@ const App = ({ element, props }) => {
     </AppContext.Provider>
   )
 }
-
 
 export default App
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,24 @@ import Image from "../components/Image"
 import SEO from "../components/SEO"
 import MessageSend from "../components/MessageSend"
 
-import { AppContext } from '../App'
+import { AppContext } from "../App"
 
 const IndexPage = () => {
-  const { peer } = useContext(AppContext);
+  const { peer } = useContext(AppContext)
 
   console.log(peer)
 
   return (
-  <>
-    <SEO title="Home" />
-    <h1>Send a message to your secret friend</h1>
-    <p>You will need to stay connected in order for the message to be sent when your secret friend opens the secret link.</p>
-    <MessageSend />
-  </>
-)}
+    <>
+      <SEO title="Home" />
+      <h1>Send a message to your secret friend</h1>
+      <p>
+        You will need to stay connected in order for the message to be sent when
+        your secret friend opens the secret link.
+      </p>
+      <MessageSend />
+    </>
+  )
+}
 
 export default IndexPage
