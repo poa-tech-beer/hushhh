89deb053573ca459bf97489f491641dd6d3478b6
Adrien Denat <adrien.denat@gmail.com>
2019-10-11 00:16:30 -0300

    chore: bring back peerjs lifecycle functions

--- src/services/p2p.js
+++ src/services/p2p.js
@@ -3,53 +3,54 @@
 // // See https://github.com/peers/peerjs/blob/master/examples/index.jsx
 
 export const getPeer = async () => {
+  let lastPeerId = null
   const Peer = await import(/* webpackChunkName: "peerjs" */ `peerjs`).then(
     module => module.default
   )
 
-  return new Peer(null, {
+  const peer = new Peer(null, {
     debug: 2,
   })
+
+  /**
+   * Connected event handler.
+   */
+  peer.on("open", function(id) {
+    // Workaround for peer.reconnect deleting previous id
+    if (peer.id === null) {
+      console.log("Received null id from peer open")
+      peer.id = lastPeerId
+    } else {
+      lastPeerId = peer.id
+    }
+    console.log("ID: " + peer.id)
+  })
+
+  /**
+   * Disconnected event handler.
+   */
+  peer.on("disconnected", function() {
+    console.log("Connection lost. Please reconnect")
+    // Workaround for peer.reconnect deleting previous id
+    peer.id = lastPeerId
+    peer._lastServerId = lastPeerId
+    peer.reconnect()
+  })
+
+  /**
+   * Closed (destroyed) connexion.
+   */
+  peer.on("close", function() {
+    console.log("Connection destroyed")
+  })
+
+  /**
+   * Error event handler.
+   */
+  peer.on("error", function(err) {
+    console.log(err)
+    alert("" + err)
+  })
+
+  return peer
 }
-
-// var lastPeerId = null
-
-// /**
-//  * Connected event handler.
-//  */
-// peer.on("open", function(id) {
-//   // Workaround for peer.reconnect deleting previous id
-//   if (peer.id === null) {
-//     console.log("Received null id from peer open")
-//     peer.id = lastPeerId
-//   } else {
-//     lastPeerId = peer.id
-//   }
-//   console.log("ID: " + peer.id)
-// })
-
-// /**
-//  * Disconnected event handler.
-//  */
-// peer.on("disconnected", function() {
-//   console.log("Connection lost. Please reconnect")
-//   // Workaround for peer.reconnect deleting previous id
-//   peer.id = lastPeerId
-//   peer._lastServerId = lastPeerId
-//   peer.reconnect()
-// })
-
-// /**
-//  * Closed (destroyed) connexion.
-//  */
-// peer.on("close", function() {
-//   console.log("Connection destroyed")
-// })
-
-// /**
-//  * Error event handler.
-//  */
-// peer.on("error", function(err) {
-//   console.log(err)
-//   alert("" + err)
-// })
